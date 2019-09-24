8652872da5f43f6637c3df887fb334b03ca01be8
Adrian Benavides <adri.benavides312@gmail.com>
2019-09-24 16:40:55 +0200

    add lora pin configuration for arduino

--- include/lora.h
+++ include/lora.h
@@ -3,11 +3,12 @@
 #define RFM95_RST D0 // RST
 #define RFM95_INT D1 // DIO0
 #define RFM95_LED D2
-
-// PIN_SPI_SCK  (14) -> D5
-// PIN_SPI_MISO (12) -> D6
-// PIN_SPI_MOSI (13) -> D7
-// PIN_SPI_SS   (15) -> D8
+//                  NodeMcu    UNO, Nano
+// -------------|------------|-----------|
+// PIN_SPI_SCK  | (14) -> D5 |    D13    |
+// PIN_SPI_MISO | (12) -> D6 |    D12    |
+// PIN_SPI_MOSI | (13) -> D7 |    D11    |
+// PIN_SPI_SS   | (15) -> D8 |    D10    |
 
 // Must match RX's freq
 #define RF95_FREQ 434.0
